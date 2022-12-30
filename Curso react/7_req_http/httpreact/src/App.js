50e8ccac988dbd05b9f27d87d1aa1689186729e7
Wesley <wesleycariutaba@gmail.com>
2022-12-30 13:18:10 -0300

    seção 7 2

--- Curso react/7_req_http/httpreact/src/App.js
+++ Curso react/7_req_http/httpreact/src/App.js
@@ -15,7 +15,8 @@ function App() {
 
   //  4 custom hook
   //se fosse por exemplo pegar dados de um usuario poderia reutilizar esse hook., passando so a url desejada.
-  const {data: items} = useFetch(url)
+  //recebe a data e o httpConfig
+  const {data: items, httpConfig, loading, error} = useFetch(url)
 
   const [name,setName] = useState("")
   const [price,setPrice] = useState("")
@@ -39,32 +40,50 @@ function App() {
       name, //como tem o mesmo nome dos estados nao precisa igular name: name
       price,
     }
-    const res = await fetch(url,{
-      method: 'POST', //por padrão é get
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(product), //passar o objeto em formato json
-    })
-    // 3 - carregamento dinâmico
-    const addedProduct = await res.json()
-    //nao poderia simplesmente colocar o product no lugar do addedProduct não? testei e funcionou
-    setProducts((prevProducts)=>[...prevProducts,addedProduct])
+    // const res = await fetch(url,{
+    //   method: 'POST', //por padrão é get
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(product), //passar o objeto em formato json
+    // })
+    // // 3 - carregamento dinâmico
+    // const addedProduct = await res.json()
+    // //nao poderia simplesmente colocar o product no lugar do addedProduct não? testei e funcionou
+    // setProducts((prevProducts)=>[...prevProducts,addedProduct])
+    // 5 refatorando post
+    //chama o useFetch dentro dele há a função de usar o POST, com a função httpConfig
+    httpConfig(product, "POST")
+
     setName("")
     setPrice("")
+  }
+
+  //8 - desafio 6
+  const handleRemove = (id) =>{
+    httpConfig(id, 'DELETE')
+
   }
 
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
-      <ul>
-      {/*antes era products no lugar de items */}
-        {items && items.map((product)=>(
-          <li key={product.id}>
-            {product.name} - R$: {product.price}
-          </li>
-        ))}
-      </ul>
+      {/* 6 - loading */}
+      {/* se carregando mostra carregando dados se não mostra os dados */}
+      {loading && <p>Carregando dados</p>}
+      {/* 8 - tratando erros */}
+      {error && <p>{error}</p>}
+      {!error && (
+        <ul>
+        {/*antes era products no lugar de items */}
+          {items && items.map((product)=>(
+            <li key={product.id}>
+              {product.name} - R$: {product.price}
+              <button onClick={() => handleRemove(product.id)}>Excluir</button>
+            </li>
+          ))}
+        </ul>
+      )}
       <div className="add-product">
         <form onSubmit={handleSubmit}>
           <label>
@@ -75,7 +94,10 @@ function App() {
             Preço:
             <input type="number" name="price" value={price} onChange={(e) => setPrice(e.target.value)}/>
           </label>
-          <input type="submit" value="Criar" />
+          {/* 7 - state de loading no post */}
+          {/* Não mostra o botão de submeter enquanto está carregando ou desabilita o botão */}
+          {loading && <input type="submit" disabled value="Aguarde" />}
+          {!loading && <input type="submit" value="Criar" />}
         </form>
       </div>
     </div>
--- Curso react/7_req_http/httpreact/src/hooks/useFetch.js
+++ Curso react/7_req_http/httpreact/src/hooks/useFetch.js
@@ -1,21 +1,113 @@
 import { useState, useEffect } from "react";
 
+
+
+
+
+
 //  4 - costum hook
 //hook é basicamente uma função
 
 export const useFetch = (url) =>{
   const [data,setData] = useState(null)
 
+  // 5 - refatorando post
+  //cabeçalhos
+  const [config, setConfig] = useState(null)
+  //metodos utilizados na função get ou post
+  const [method, setMethod] = useState(null)
+  //dado inserido no sistema, chama o callFetch
+  const [callFetch, setCallFetch] = useState(false)
+
+  //6 loading
+  //não está carregando nada
+  const [loading, setLoading] = useState(false)
+
+  // 7 - tratando erros
+  const [error, setError] = useState(null)
+
+  // 8 - desafio 6
+  const [itemId, setItemId] = useState(null)
+
+  //função para organizar a configuração do POST
+  const httpConfig = (data, method) => {
+    if(method === "POST"){
+      setConfig({
+        method,
+        headers: {
+          'Content-type' : 'application/json'
+        },
+        body: JSON.stringify(data),
+      })
+      setMethod(method)
+    }
+    // Tarefa 6
+    else if(method === "DELETE"){
+      setConfig({
+        method,
+        headers: {
+          'Content-type' : 'application/json'
+        },
+      })
+      setMethod(method)
+      setItemId(data)
+    }
+  }
+
+  //useEffect usado para get, pegar os dados do arquivo, caso a url mude ou o callFetch
   useEffect(()=>{
     const fetchData = async ()=>{
-      const res = await fetch(url)
-      const json = await res.json()
-      setData(json)
 
+      //6 loading
+      //está carregando os dados
+      setLoading(true)
+      // 7 - tratando erros
+      try {
+        const res = await fetch(url)
+        const json = await res.json()
+        setData(json)  
+      } catch (error) {
+        console.log(error.message)
+        setError("Houve algum erro ao carregador os dados!") 
+      }
+      //dados já carregados
+      setLoading(false)
     }
 
     fetchData()
-  },[url])
+  },[url, callFetch])
+
+  // 5 - refatorando post
+  //useEffect ativado quando o config, method ou url é alterado
+  useEffect(()=>{
+    const httpRequest = async () =>{
+      let json
+      if(method === "POST"){
+        //faz o POST com as configurações geradas pela função anteriormente, a httpConfig
+        //ela altera o config e o method
+        let fetchOptions = [url, config]
+        //porque o spred operator?
+        //insere no db.json
+        const res = await fetch(...fetchOptions)
+        //recebe como resposta
+        json = await res.json()
+        //seta o callFetch, ativando assim o useEffect do fetchData que faz um post, setando os Datas e retornando em return data httpconfig para ser exibido.
+        // setCallFetch(json)
+      }
+      else if(method === "DELETE"){
+        //tarefa 6
+        const deleteUrl = `${url}/${itemId}`
+        // console.log(deleteUrl)
+        const res = await fetch(deleteUrl, config)
+
+        json = await res.json()
+
+      }
+      setCallFetch(json)
+    }
+    httpRequest()
+  }, [config, method, url])
+
   //dados que vai utilizar no hook
-  return {data}
+  return {data, httpConfig, loading, error}
 }
